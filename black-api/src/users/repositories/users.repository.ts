95ffb2db35503f7578c3d2f13c36247529e763fb
fersilverio <ssilveriofernando45@gmail.com>
2025-04-14 23:32:56 -0300

    Creating initial structure to ecosystem

---
+++ black-api/src/users/repositories/users.repository.ts
@@ -0,0 +1,11 @@
+import { CreateUserDto } from "../dto/create-user.dto";
+import { UpdateUserDto } from "../dto/update-user.dto";
+import { User } from "../entities/user.entity"
+
+export interface UsersRepository {
+    create(data: CreateUserDto): Promise<User>;
+    findOne(id: number): Promise<User>;
+    findAll(): Promise<User[]>;
+    update(id: number, data: UpdateUserDto): Promise<User>;
+    delete(id: number): Promise<User>;
+}
